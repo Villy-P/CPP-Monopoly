794aefb67615ce85daa6000a39b325a763ddef9d
Villy-P <valeriuspetrini@gmail.com>
2022-09-27 14:38:46 -0500

    cardManagment start

---
+++ src/cardManagment.cpp
@@ -0,0 +1,12 @@
+#ifndef CARD_MANAGMENT_CPP
+#define CARD_MANAGMENT_CPP
+
+#include "cardManagment.hpp"
+
+#include <algorithm>
+
+void cardManagment::CardManagment::shuffleDecks() {
+
+}
+
+#endif
---
+++ src/cardManagment.hpp
@@ -0,0 +1,18 @@
+#ifndef CARD_MANAGMENT_HPP
+#define CARD_MANAGMENT_HPP
+
+#include <vector>
+
+#include "card.hpp"
+
+namespace card_managment {
+    class CardManagment {
+        public: 
+            std::vector<card::Card> chanceCards;
+            std::vector<card::Card> chestCards;
+
+            void shuffleDecks();
+    };
+}
+
+#endif
