80a3bb6ddd0b0570761d50423bdefe29775ac3d1
Villy-P <valeriuspetrini@gmail.com>
2022-09-29 14:40:19 -0500

    display chance card

--- src/cardManagment.cpp
+++ src/cardManagment.cpp
@@ -13,8 +13,6 @@ void card_managment::CardManagment::shuffleDecks() {
     std::default_random_engine rng = std::default_random_engine {};
     std::shuffle(this->chanceCards.begin(), this->chanceCards.end(), rng);
     std::shuffle(std::begin(this->chestCards), std::end(this->chestCards), rng);
-    for (auto c : this->chanceCards)
-        std::cout << c.description << std::endl;
 }
 
 void card_managment::CardManagment::createCards() {
@@ -50,34 +48,30 @@ void card_managment::CardManagment::drawChanceCard() {
     std::vector<std::string> displaySentence = functions::split(card.description, ' ');
     unsigned char lengthOfEach = displaySentence.size() / 5;
     unsigned char extraLetters = displaySentence.size() % 5;
-    std::cout << "+------------------------------+" << std::endl;
-    std::cout << "|            OOOOOO            |" << std::endl;
-    std::cout << "|            O    O            |" << std::endl;
-    std::cout << "|              OOOO            |" << std::endl;
-    std::cout << "|              O               |" << std::endl;
-    std::cout << "|              .               |" << std::endl;
-    std::cout << "+------------------------------+" << std::endl;
+    std::cout << "+---------------------------------------------+" << std::endl;
+    std::cout << "|                    OOOOOO                   |" << std::endl;
+    std::cout << "|                    O    O                   |" << std::endl;
+    std::cout << "|                      OOOO                   |" << std::endl;
+    std::cout << "|                      O                      |" << std::endl;
+    std::cout << "|                      .                      |" << std::endl;
+    std::cout << "+---------------------------------------------+" << std::endl;
     functions::readStringInput("");
-    std::cout << "+----------------------------+" << std::endl;
-    char count = -1;
-    for (unsigned char i = 0; i < 5; i++) {
-        count++;
-        std::string str = "";
-        for (unsigned char j = 0; j < lengthOfEach; j++) {
-            str += displaySentence[j + count] + " ";
-            if (extraLetters > 0) {
-                count++;
-                str += displaySentence[j + count] += " ";
-                extraLetters--;
-            }
+    std::cout << "+----------------------------------------------+" << std::endl;
+    for (unsigned char start = 0, end = lengthOfEach; start < displaySentence.size(); start = end, end = start + lengthOfEach) {
+        if (extraLetters) {
+            end++;
+            extraLetters--;
         }
+        std::string str = "";
+        for (unsigned char i = start; i < end; i++)
+            str +=  displaySentence[i] + " ";
         str.pop_back();
-        std::cout << "|" << functions::center(str, 28) << "|" << std::endl;
-        count++;
+        std::cout << "|" << functions::center(str, 46) << "|" << std::endl;
     }
-    std::cout << "+----------------------------+" << std::endl;
-    this->chanceCards.erase(this->chanceCards.begin());
+    std::cout << "+----------------------------------------------+" << std::endl;
+    this->chanceCards.pop_front();
     this->chanceCards.push_back(card);
+    std::cout << this->chanceCards.size() << std::endl;
 }
 
 #endif
--- src/cardManagment.hpp
+++ src/cardManagment.hpp
@@ -1,15 +1,15 @@
 #ifndef CARD_MANAGMENT_HPP
 #define CARD_MANAGMENT_HPP
 
-#include <vector>
+#include <deque>
 
 #include "card.hpp"
 
 namespace card_managment {
     class CardManagment {
         public: 
-            std::vector<card::Card> chanceCards;
-            std::vector<card::Card> chestCards;
+            std::deque<card::Card> chanceCards;
+            std::deque<card::Card> chestCards;
 
             void shuffleDecks();
             void drawChanceCard();
